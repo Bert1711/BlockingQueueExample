06211be53928eba8b99eaa56fd87ed947d528b80
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-03 22:50:51 +0300

    Initial commit

---
+++ src/main/java/com/zaroyan/Main.java
@@ -0,0 +1,12 @@
+package com.zaroyan;
+
+/**
+ * 
+ * @author Zaroyan
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
