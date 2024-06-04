a1951713bd642d13e3970a95860bf5dee82c5a86
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-06-04 13:15:50 +0300

    "initial commit"

--- src/main/java/com/zaroyan/BlockingQueue.java
+++ src/main/java/com/zaroyan/BlockingQueue.java
@@ -1,7 +1,38 @@
-package com.zaroyan;/**
- * 
+package com.zaroyan;
+import java.util.LinkedList;
+
+/**
  * @author Zaroyan
- * 
  */
-public class BlockingQueue {
+
+public class BlockingQueue<T> {
+    private LinkedList<T> queue;
+    private int capacity;
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+        this.queue = new LinkedList<>();
+    }
+
+    public synchronized void enqueue(T item) throws InterruptedException {
+        while (queue.size() == capacity) {
+            wait(); // Ждем, пока не освободится место в очереди
+        }
+        queue.add(item);
+        notifyAll(); // Сигнализируем о том, что появился новый элемент
+    }
+
+    public synchronized T dequeue() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait(); // Ждем, пока не появится новый элемент в очереди
+        }
+        T item = queue.remove();
+        notifyAll(); // Сигнализируем о том, что элемент был извлечен
+        return item;
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
 }
+
--- src/main/java/com/zaroyan/Main.java
+++ src/main/java/com/zaroyan/Main.java
@@ -1,12 +1,42 @@
 package com.zaroyan;
+import java.util.LinkedList;
 
 /**
- * 
  * @author Zaroyan
- * 
  */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BlockingQueue<Integer> queue = new BlockingQueue<>(5);
+
+
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 1; i <= 10; i++) {
+                    queue.enqueue(i);
+                    System.out.println("Produced: " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        Thread consumer = new Thread(() -> {
+            try {
+                for (int i = 1; i <= 10; i++) {
+                    int item = queue.dequeue();
+                    System.out.println("Consumed: " + item);
+                    Thread.sleep(2000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
     }
 }
+
